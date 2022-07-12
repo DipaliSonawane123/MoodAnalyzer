921c0b23bd850fbce9d16a5d0d6c2c08de39def5
Dipali <dipssonawane123@gmail.com>
2022-07-12 22:23:48 +0530

    Modified :handle null exception feature is modified

--- src/main/java/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/moodanalyzer/MoodAnalyzer.java
@@ -1,22 +1,24 @@
 package moodanalyzer;
 
 public class MoodAnalyzer {
-
     private String message;
+    //Default Constructor
+    public MoodAnalyzer() {
+    }
+    //Parameterized Constructor
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
 
-        //Default Constructor
-        public MoodAnalyzer() {
-        }
-
-        //Parameterized Constructor
-        public MoodAnalyzer(String message) {
-            this.message = message;
-        }
-
-        public String analyseMood() {
+    //Handle NULLPOINTER Exception using try-catch block
+    public String analyseMood() {
+        try {
             if (message.contains("sad"))
                 return "SAD";
             else
                 return "HAPPY";
+        } catch(Exception e) {
+            return "HAPPY";
         }
     }
+}
--- src/test/java/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/moodanalyzer/MoodAnalyzerTest.java
@@ -5,19 +5,24 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
 
-
-    @Test
-    public void testMoodAnalysis_whenMoodIsSad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");//Tc1.1 Refactor
+    public void testMoodAnalysis_whenMoodIsSad() { //tc 1.1 Iamin sad mood
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
         String mood = moodAnalyzer.analyseMood();
         Assertions.assertEquals(mood,"SAD");
     }
 
     @Test
-    public void testMoodAnalysis_whenMoodIsHappy() {//Tc1.2 Refactor
+    public void testMoodAnalysis_whenMoodIsHappy() {//tc 1.2 Iamin sad mood
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
         String mood1 = moodAnalyzer.analyseMood();
         Assertions.assertEquals(mood1,"HAPPY");
+
+    }
+    @Test
+    public void testMoodAnalysis_whenMoodIsNull() { //tc 2.1
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood2 = moodAnalyzer.analyseMood();
+        Assertions.assertEquals(mood2,"HAPPY");
 
     }
 }
